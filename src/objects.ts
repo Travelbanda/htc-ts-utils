ded765a2c45f1874ae79a7a19c0ee5a610e7e2b1
ShevtsovAleksey <proshevtsov@yandex.ru>
2018-07-11 11:16:48 +0300

    initial commit

--- src/objects.ts
+++
@@ -1,9 +0,0 @@
-export function filterObject<T extends { [o: string]: any }>(obj: T) {
-  const ret: T = {} as any
-
-  Object.keys(obj)
-    .filter((key) => obj[key] !== undefined)
-    .forEach((key) => ret[key] = obj[key])
-
-  return ret
-}
